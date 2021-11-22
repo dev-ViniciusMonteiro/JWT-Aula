499575f59860bc174ad0adc38b2023c6a498c678
Vinicius Monteiro Orlandi <vinicius.orlandi@pampa.compasso>
2021-11-22 20:23:33 -0300

    ver2.0

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,15 +1,32 @@
+const db = require("../models");
+const User = db.user;
+
 exports.allAccess = (req, res) => {
-  res.status(200).send("Public Content.");
+    res.status(200).send("Public Content.");
 };
 
-exports.userBoard = (req, res) => {
-  res.status(200).send("User Content.");
+exports.userBoard = async(req, res) => {
+    var array = []
+    for (i = 0; i < 50; i++) {
+        let user = await User.findOne({
+            where: {
+                username: i
+            }
+        })
+        if (user != null) {
+            array.push(user)
+        }
+    }
+
+
+    res.send({ user: array })
+
 };
 
 exports.adminBoard = (req, res) => {
-  res.status(200).send("Admin Content.");
+    res.status(200).send("Admin Content.");
 };
 
 exports.moderatorBoard = (req, res) => {
-  res.status(200).send("Moderator Content.");
+    res.status(200).send("Moderator Content.");
 };
